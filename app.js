6fe095c85c88d1701244d75fbe95c944771efc85
tacia68 <tacia68@gmail.com>
2023-08-20 00:16:11 -0400

    add arquivos

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bem vindos ao WebAcademy!!');
+});
+
+app.listen(8083, () => {
+  console.log('Rodando na porta 8083');
+});
